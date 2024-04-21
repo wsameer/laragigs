db5a371cc96c10edf1989b9673e7b122dd05bb74
wsameer <dev.wsameer@gmail.com>
2024-04-21 18:33:51 -0400

    adds a pagination component

---
+++ resources/js/Components/Pagination.jsx
@@ -0,0 +1,30 @@
+import { Link } from '@inertiajs/react';
+import React from 'react';
+
+const Pagination = React.memo(({ links }) => {
+  console.log('🚀 ~ Pagination ~ links:', links);
+
+  return (
+    <nav className="text-center mt-4">
+      {links.map(link => (
+        <Link
+          preserveScroll
+          href={link.url || ''}
+          key={link.label}
+          className={
+            'inline-block mx-1 py-2 px-3 rounded-lg text-gray-900 text-xs ' +
+            (link.active
+              ? 'text-gray-300 bg-gray-200 cursor-not-allowed'
+              : ' ') +
+            (!link.url
+              ? '!text-gray-500 cursor-not-allowed'
+              : 'hover:bg-gray-200')
+          }
+          dangerouslySetInnerHTML={{ __html: link.label }}
+        ></Link>
+      ))}
+    </nav>
+  );
+});
+
+export default Pagination;
