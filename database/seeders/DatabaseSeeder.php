a249f4a4f25ec6310450d2835a37069a8135f7dc
wsameer <dev.wsameer@gmail.com>
2024-04-21 15:11:20 -0400

    adds migration files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,30 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    User::factory()->create([
+      'name' => 'Sameer',
+      "email" => 'sam@test.com',
+      'email' => bcrypt('parallel900'),
+      "email_verified_at" => time()
+    ]);
+
+    Project::factory()
+      ->count(15)
+      ->hasTasks(15)
+      ->create();
+  }
 }
