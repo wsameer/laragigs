6a8598187bf1809bc9dc8c5016f3dfea51c22681
wsameer <dev.wsameer@gmail.com>
2024-04-21 17:06:16 -0400

    changes entry point route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Inertia\Inertia;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +18,34 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-Route::get('/', function () {
-  return Inertia::render('Welcome', [
-    'canLogin' => Route::has('login'),
-    'canRegister' => Route::has('register'),
-    'laravelVersion' => Application::VERSION,
-    'phpVersion' => PHP_VERSION,
-  ]);
+// Route::get('/', function () {
+//   return Inertia::render('Welcome', [
+//     'canLogin' => Route::has('login'),
+//     'canRegister' => Route::has('register'),
+//     'laravelVersion' => Application::VERSION,
+//     'phpVersion' => PHP_VERSION,
+//   ]);
+// });
+
+Route::redirect('/', '/dashboard', 301);
+
+Route::middleware(['auth', 'verified'])->group(function () {
+	Route::get('/dashboard', fn () => Inertia::render('Dashboard'))
+		->name('dashboard');
+
+	Route::resource('project', ProjectController::class);
+	Route::resource('task', TaskController::class);
+	Route::resource('user', UserController::class);
 });
 
 Route::get('/dashboard', function () {
-  return Inertia::render('Dashboard');
+	return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
