5a43b3174e808515f26508e962cdb0302d96c3c0
wsameer <dev.wsameer@gmail.com>
2024-04-28 11:23:24 -0400

    table rending logic component

---
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TasksTable = ({ tasks, queryParams }) => {
+  queryParams = queryParams || {};
+
+  return <div>TasksTable</div>;
+};
+
+export default TasksTable;
