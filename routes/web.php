5d08e1858b5d5cae38d8ea88efe3e63d7b370fe4
wsameer <dev.wsameer@gmail.com>
2024-04-27 23:32:02 -0400

    clean up routes

--- routes/web.php
+++ routes/web.php
@@ -18,34 +18,20 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::get('/', function () {
-//   return Inertia::render('Welcome', [
-//     'canLogin' => Route::has('login'),
-//     'canRegister' => Route::has('register'),
-//     'laravelVersion' => Application::VERSION,
-//     'phpVersion' => PHP_VERSION,
-//   ]);
-// });
-
 Route::redirect('/', '/dashboard', 301);
 
 Route::middleware(['auth', 'verified'])->group(function () {
-	Route::get('/dashboard', fn () => Inertia::render('Dashboard'))
-		->name('dashboard');
+  Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
 
-	Route::resource('project', ProjectController::class);
-	Route::resource('task', TaskController::class);
-	Route::resource('user', UserController::class);
+  Route::resource('project', ProjectController::class);
+  Route::resource('task', TaskController::class);
+  Route::resource('user', UserController::class);
 });
 
-Route::get('/dashboard', function () {
-	return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
